15634395f88cf0c29995cd31a3e40b7be1b09946
kramstyles <kramstyles@outlook.com>
2023-02-17 17:09:19 +0100

    added stacks and load more button

--- src/Components/TechStack/index.jsx
+++ src/Components/TechStack/index.jsx
@@ -1,4 +1,5 @@
 import "./index.css";
+import {CgAnchor} from "react-icons/cg";
 
 const TechStack = () => {
     const techStack = [
@@ -11,13 +12,29 @@ const TechStack = () => {
             <div className="line-title"/>
             <div className="row my-5 justify-content-center">
                 {techStack.map((item, index) => {
+                    let randomNumber = Math.random().toFixed(2);
+
+                    // Added so that random black colors won't be too transparent
+                    if (randomNumber <= 0.35) randomNumber = parseInt(randomNumber) + 0.35
+
+                    const randomColorGen = `rgba(0, 0, 0, ${randomNumber})`;
                     return (
                         <div
-                            className="col-md-3 col-sm-6 col-xs-12 text-center poppins shadow px-5 py-4 m-3 tech-stack transit-fast">
+                            className="col-lg-3 col-md-5 mx-md-1 col-sm-12 text-center poppins shadow px-5 py-4
+                            m-lg-3 my-4 tech-stack transit-fast position-relative"
+                            key={index}>
                             {item}
+                            <p className="tech-index text-white"
+                               style={{backgroundColor: randomColorGen}}>{index + 1}</p>
                         </div>
                     )
                 })}
+            </div>
+            <div className="row">
+                <div className="col-6 offset-3">
+                    <button type="button" className="btn btn-dark btn-sm m-auto d-block button">Load More <span
+                    className="btn-icon transit-fast"><CgAnchor/></span></button>
+                </div>
             </div>
         </div>
     )
