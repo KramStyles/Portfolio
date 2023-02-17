6e3cbff510aaf3c3ce5ac8a524ecaff01a50474e
kramstyles <kramstyles@outlook.com>
2023-02-17 23:27:13 +0100

    changed to dark and made about responsive

--- src/Components/TechStack/index.jsx
+++ src/Components/TechStack/index.jsx
@@ -1,17 +1,25 @@
-import "./index.css";
+import {useState} from "react";
 import {CgAnchor} from "react-icons/cg";
 
+import "./index.css";
+
 const TechStack = () => {
+    const [showMoreStack, setShowMoreStack] = useState(9);
     const techStack = [
-        "HTML", "CSS", "Python", "JavaScript", "React Js", "AJAX", "Angular Js", "TypeScript", "CI/CD",
-        "Flask", "Django", "Django Rest", "JQuery", "PostgreSQL", "MySQL", "SQlite3", "Docker",
+        "HTML", "CSS", "Python", "JavaScript", "React Js", "Django", "Django Rest", "Flask", "CI/CD",
+        "PostgreSQL", "MySQL", "SQlite3", "Docker", "AJAX", "Angular Js", "TypeScript", "JQuery",
     ]
+    const loadMore = () => {
+        setShowMoreStack((prevState => (
+            prevState + 3
+        )))
+    }
     return (
         <div className="container shadow section">
             <h3 className="">Tech Stack</h3>
             <div className="line-title"/>
             <div className="row my-5 justify-content-center">
-                {techStack.map((item, index) => {
+                {techStack.slice(0, showMoreStack).map((item, index) => {
                     let randomNumber = Math.random().toFixed(2);
 
                     // Added so that random black colors won't be too transparent
@@ -30,12 +38,15 @@ const TechStack = () => {
                     )
                 })}
             </div>
-            <div className="row">
-                <div className="col-6 offset-3">
-                    <button type="button" className="btn btn-dark btn-sm m-auto d-block button">Load More <span
-                    className="btn-icon transit-fast"><CgAnchor/></span></button>
+            {showMoreStack >= techStack.length ? null : (
+                <div className="row">
+                    <div className="col-6 offset-3">
+                        <button type="button" className="btn btn-dark btn-sm m-auto d-block button"
+                                onClick={loadMore}>Load More <span
+                            className="btn-icon transit-fast"><CgAnchor/></span></button>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
