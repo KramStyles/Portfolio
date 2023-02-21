e5d7d8f84f778f4719d3fe76f365ca0cff5bb0e1
kramstyles <kramstyles@outlook.com>
2023-02-21 22:56:35 +0100

    introduced experience

---
+++ src/Components/Experience/index.jsx
@@ -0,0 +1,11 @@
+import Title from "../Elements/title";
+
+const Experience = () => {
+    return (
+        <div className="container shadow section position-relative">
+            <Title backgroundTitle="jobs" title="Experience"/>
+        </div>
+    )
+}
+
+export default Experience;
