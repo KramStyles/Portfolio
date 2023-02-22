c8050b6797b0c2df69a8d4b4303d1e3d39939283
kramstyles <kramstyles@outlook.com>
2023-02-22 20:31:39 +0100

    error at vertical button

--- src/Components/Experience/index.jsx
+++ src/Components/Experience/index.jsx
@@ -10,6 +10,12 @@ import Buttons from "../Elements/buttons";
 
 const Experience = () => {
     const [more, setMore] = useState(false);
+
+    const checkMore = () => {
+        setMore((prevState => (
+            !prevState
+        )));
+    }
     return (
         <div className="container shadow section position-relative">
             <Title backgroundTitle="work" title="Experience"/>
@@ -20,7 +26,8 @@ const Experience = () => {
                         {background: 'silver', color: '#666'},
                         {background: 'grey', color: 'snow'},
                     ]
-                    const iconStyle = styleChoices[index%2];
+                    const iconStyle = styleChoices[index % 2];
+
                     return (
                         <VerticalTimelineElement
                             className="vertical-timeline-element--work"
@@ -32,43 +39,15 @@ const Experience = () => {
                         >
                             <h3 className="vertical-timeline-element-title text-start">{item.name}</h3>
                             <h4 className="vertical-timeline-element-subtitle">{item.role}</h4>
-                            <div dangerouslySetInnerHTML={{__html: item.desc}} className={"exp-item-content"}/>
-                            <Buttons btnIcon={<CgStark/>} btnText={"Show More"} moreClass={"btn-outline-dark"}/>
+                            <div dangerouslySetInnerHTML={{__html: item.desc}}
+                                 className={`exp-item-content transit-fast ${more ? "exp-height" : ""}`}/>
+                            {item.desc.length >= 150 ? (
+                                <Buttons btnIcon={<CgStark/>} btnText={"Show More"}
+                                         moreClass={"btn-outline-dark mt-3"} onclick={checkMore}/>
+                            ) : null}
                         </VerticalTimelineElement>
                     )
                 })}
-            </VerticalTimeline>
-
-            <VerticalTimeline>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{background: 'var(--bg-gradient-light)', color: 'var()'}}
-                    contentArrowStyle={{borderRight: '7px solid white'}}
-                    date="2011 - present"
-                    dateClassName="vertical-date"
-                    iconStyle={{background: 'grey', color: 'snow'}}
-                    icon={<CgWorkAlt/>}
-                >
-                    <h3 className="vertical-timeline-element-title text-start">Creative Director</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-                    <p>
-                        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2010 - 2011"
-                    iconStyle={{background: 'silver', color: '#666'}}
-                    icon={<CgWorkAlt/>}
-                    dateClassName="vertical-date"
-                >
-                    <h3 className="vertical-timeline-element-title text-start">Art Director</h3>
-                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                    <p>
-                        Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-                    </p>
-                </VerticalTimelineElement>
-
                 <VerticalTimelineElement
                     iconStyle={{background: 'white', color: 'black', border: '2px double black'}}
                     icon={<CgStark/>}
