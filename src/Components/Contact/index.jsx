d3d0bc61e6a0a9e238cbe9883a027d1f907728d5
kramstyles <kramstyles@outlook.com>
2023-02-25 15:48:52 +0100

    created form controls

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -1,7 +1,10 @@
-import Title from "../Elements/title";
+import {CgModem} from "react-icons/cg";
 
-const Contact = () =>{
-    return(
+import Title from "../Elements/title";
+import Buttons from "../Elements/buttons";
+
+const Contact = () => {
+    return (
         <div className="container shadow my-shadow-light section bg-linear-dark mb-5">
             <Title backgroundTitle="contact" title="Reach Me"/>
             <div className="row mt-3">
@@ -10,7 +13,25 @@ const Contact = () =>{
                 </div>
                 <div className="col-md-8">
                     <div className="glass-morphic-content">
-                        hello
+                        <form>
+                            <div className="mb-3 mt-3">
+                                <input type="text" className="form-control" id="fname" placeholder="Fullname"
+                                       name="fname"/>
+                            </div>
+                            <div className="mb-3 mt-3">
+                                <input type="email" className="form-control" id="email" placeholder="Enter email"
+                                       name="email"/>
+                            </div>
+                            <div className="mb-3 mt-3">
+                                <input type="tel" className="form-control" id="phone" placeholder="Mobile (Optional)"
+                                       name="phone"/>
+                            </div>
+                            <div className="mb-3">
+                                <textarea className="form-control" id="message" name="message"
+                                          placeholder="Enter message" rows={5}/>
+                            </div>
+                            <Buttons btnText="Send Message" btnIcon={<CgModem/>}/>
+                        </form>
                     </div>
                 </div>
             </div>
