4c0421311b02f34949e3397ee153b5534b78fd4c
kramstyles <kramstyles@outlook.com>
2023-03-01 15:13:22 +0100

    added my preloader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Home from "./Components/Home";
 import Sidebar from "./Components/Sidebar";
@@ -9,29 +9,40 @@ import Testimonials from "./Components/Testimonials";
 import Experience from "./Components/Experience";
 import Contact from "./Components/Contact";
 import ScrollToTop from "react-scroll-to-top";
-import {CgArrowLongUpE} from "react-icons/cg";
+import { CgArrowLongUpE } from "react-icons/cg";
 
 import "./App.css";
 
 const App = () => {
-    return (
+    const [loading, setloading] = useState(true);
+    const preloader = document.querySelector(".my-preloader");
+
+    // setTimeout(() => {
+    //     preloader.style.opacity = "0";
+    //     setloading(true);
+    // }, 2000);
+
+    return loading ? (
+        null
+    ) : ( 
         <>
-            <Sidebar/>
-            <Home/>
-            <About/>
-            <Experience/>
-            <TechStack/>
-            <Project/>
-            <Testimonials/>
-            <Contact/>
+            <Sidebar />
+            <Home />
+            <About />
+            <Experience />
+            <TechStack />
+            <Project />
+            <Testimonials />
+            <Contact />
             <ScrollToTop
                 smooth={true}
-                component={<CgArrowLongUpE/>}
+                component={<CgArrowLongUpE />}
                 className={"vibrate"}
-                style={{right: 10}}
+                style={{ right: 10 }}
             />
         </>
     )
+
 }
 
 export default App;
